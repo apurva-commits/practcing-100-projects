0c3c54467f43fc0af98b398f0a8a95d542182e2f
apurva-commits <apurvashinde908@gmail.com>
2025-02-25 16:38:03 +0530

    Add files via upload
    so yeah this is my first python "Band Name Generator project" i have  created it on PyCharm.

---
+++ first.py
@@ -0,0 +1,7 @@
+print("Welcome to the brand name Generator.")
+#this is first python project day1
+name= input("Whats the name of the City you grew up in?\n")
+#to get the cursor on new line we used backslash n
+hi=input("Whats your pet's name?\n")
+concat=(name +" " +  hi)
+print('Your band name could be '+ concat)
