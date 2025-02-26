8c5ec538959bc948227b224324f9ca57efd6f95c
apurva-commits <apurvashinde908@gmail.com>
2025-02-26 16:13:16 +0530

    Add files via upload

---
+++ treasure island.py
@@ -0,0 +1,18 @@
+print("Welcome to Treasure island!!!! all the best to find the treasure ")
+a=input("Where do you want to go LEFT or RIGHT choose carefully or you may die")
+if a=="LEFT":
+  print("great luck, you spawned on the lake")
+  b=input("Now choose what you wanna do? SWIM or WAIT")
+  if b=="WAIT":
+       print("wow you are truly a champ")
+       c=input("Now final choose the door RED, BLUE, YELLOW")
+       if c=="RED":
+           print("DEVIL have eaten you")
+       elif c=="YELLOW":
+           print("SATTT CRORE YOU WONNNNN")
+       else:
+           print("POOR YOU DIED LEFT")
+  else:
+      print("you died")
+else:
+    print("REST IN PEACE")
